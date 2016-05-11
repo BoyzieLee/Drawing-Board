60a690eb3afa9c52f63a8de8d3e50b5756014877
Boyzie Mathis <boyzielee@gmail.com>
2016-05-11 16:38:49 -0400

    Code Challenge: return an array.

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,17 +1,19 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-for (var i = 1; i < 101; i++) {
-  if (i % 15 === 0) {
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(i);
-  }
+function digitize(n) {
+
+  var convert = n.toString();
+
+  var sperate = convert.split('');
+
+  return sperate.map(function (str) {
+    return parseInt(str);
+  });
 }
+
+var x = digitize(5675676797897856765);
+console.log(x);
 
 },{}]},{},[1])
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-for (var i=1; i<101; i++) {
-  if (i % 15 === 0) {
-    console.log('FizzBuzz');
-  } else if (i % 3 === 0) {
-    console.log('Fizz');
-  } else if (i % 5 === 0) {
-    console.log('Buzz');
-  } else {
-    console.log(i);
-  }
+function digitize(n) {
+
+  var convert = n.toString();
+
+  var sperate = convert.split('');
+
+  return sperate.map(function (str){
+    return parseInt(str);
+
+  });
 }
+
+let x = digitize (5675676797897856765);
+console.log(x);
